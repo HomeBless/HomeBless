2cd7b47076fbdb7f1fc5979cae9d4bb0073320aa
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2025-02-12 18:37:49 +0700

    Register Admin

--- HomeBless/admin.py
+++ HomeBless/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Seller, Property, PropertyImage
 
 # Register your models here.
+admin.site.register(Seller)
+admin.site.register(Property)
+admin.site.register(PropertyImage)
