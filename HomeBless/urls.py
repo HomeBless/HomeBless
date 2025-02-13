f5244661f80d401f45ddeb68b29cc338fb088764
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2025-02-13 12:07:59 +0700

    link pages

--- HomeBless/urls.py
+++ HomeBless/urls.py
@@ -6,4 +6,6 @@ app_name = "HomeBless"
 urlpatterns = [
     path('', views.HomePage.as_view(), name='homepage'),
     path('property-listing/', views.PropertyListing.as_view(), name='property-listing'),
+    path('compare/', views.Compare.as_view(), name='compare'),
+    path('property-listing/<int:property_id>', views.PropertyDetail.as_view(), name='property-detail'),
 ]
--- HomeBless/views.py
+++ HomeBless/views.py
@@ -17,7 +17,37 @@ class HomePage(TemplateView):
     template_name = 'homepage.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        lines = [
+            {'name': 'สายสุขุมวิท', 'color': '#6fbe44', 'color_name': 'สีเขียว'},
+            {'name': 'สายสีลม', 'color': '#119b49', 'color_name': 'สีเขียวเข้ม'},
+            {'name': 'สายสีชมพู', 'color': '#ef5599', 'color_name': 'สีชมพู'},
+            {'name': 'สายเฉลิมรัชมงคล', 'color': '#0e76bc', 'color_name': 'สีน้ำเงิน'},
+            {'name': 'สายฉลองรัชธรรม', 'color': '#67318f', 'color_name': 'สีม่วง'},
+            {'name': 'สายสีทอง', 'color': '#d4af37', 'color_name': 'สีทอง'},
+            {'name': 'สายสีส้ม', 'color': '#ee7821', 'color_name': 'สีส้ม'},
+            {'name': 'สายสีแดง', 'color': '#d71a28', 'color_name': 'สีแดง'}
+        ]
+        return render(request, self.template_name, {'lines': lines})
 
     def post(self, request, *args, **kwargs):
         return redirect('HomeBless:homepage')
+
+
+class Compare(TemplateView):
+    template_name = 'compare.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        return redirect('HomeBless:compare')
+
+
+class PropertyDetail(TemplateView):
+    template_name = 'property-detail.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        return redirect('HomeBless:property-detail')
