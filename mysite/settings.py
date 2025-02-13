69a526379cd33d97b5c9bf6bffbb861129c44fc3
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2025-02-14 00:17:50 +0700

    move svg and adjust property listing

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'HomeBless',
-    'storages'
+    'storages',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
