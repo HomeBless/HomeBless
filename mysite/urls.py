09327560f5cc6e39fb6adc3075929fc1dc3f125c
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2025-03-14 18:16:07 +0700

    create custom login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('HomeBless.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('account/', include('allauth.urls')),
 ]
