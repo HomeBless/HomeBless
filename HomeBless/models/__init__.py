8440e6c0233f7abec6699a3e69dba01817965522
Nuafah <yasatsawin.k@ku.th>
2025-03-16 18:15:57 +0700

    Update Property Models

--- HomeBless/models/__init__.py
+++ HomeBless/models/__init__.py
@@ -1,3 +1,13 @@
-from .seller import Seller
+from .seller import Seller, seller_profile_picture_upload_path
 from .property import Property
-from .property_image import PropertyImage
+from .property_image import PropertyImage, property_image_upload_path
+from .property_type import PropertyType
+from .warranty import Warranty
+from .property_condition import PropertyCondition
+from .view import View
+from .project_amenity import ProjectAmenity
+from .ceiling import Ceiling
+from .home_feature import HomeFeature
+from .decoration import Decoration
+from .flooring import Flooring
+from .wall import Wall
--- HomeBless/models/property.py
+++ HomeBless/models/property.py
@@ -1,16 +1,18 @@
 from django.db import models
 from .seller import Seller
+from .property_type import PropertyType
+from .decoration import Decoration
+from .flooring import Flooring
+from .wall import Wall
+from .ceiling import Ceiling
+from .home_feature import HomeFeature
+from .project_amenity import ProjectAmenity
+from .property_condition import PropertyCondition
+from .view import View
+from .warranty import Warranty
 
 
 class Property(models.Model):
-    PROPERTY_TYPE_CHOICES = [
-        ('house', 'House'),
-        ('apartment', 'Apartment'),
-        ('condominium', 'Condominium'),
-        ('dorm', 'Dorm'),
-        ('land', 'Land')
-    ]
-
     SELLING_TYPE_CHOICES = [
         ('buy', 'For Sale'),
         ('rent', 'For Rent')
@@ -19,9 +21,9 @@ class Property(models.Model):
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name="properties")
     title = models.CharField(max_length=255)
     description = models.TextField()
-    property_type = models.CharField(max_length=20, choices=PROPERTY_TYPE_CHOICES)
+    property_type = models.ForeignKey(PropertyType, on_delete=models.SET_NULL, null=True, blank=True)
     transaction_type = models.CharField(max_length=10, choices=SELLING_TYPE_CHOICES, default='buy')
-    price = models.DecimalField(max_digits=12, decimal_places=2)
+    price = models.FloatField(blank=False, null=False)
     location = models.CharField(max_length=255)
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
@@ -31,8 +33,31 @@ class Property(models.Model):
     garages = models.IntegerField(null=True, blank=True)
     area = models.FloatField(help_text="Area in square wa")
     is_available = models.BooleanField(default=True)
-    construct_year = models.IntegerField(max_length=4, blank=True, null=True)
+    construct_year = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    # Decoration & Materials
+    decoration = models.ForeignKey(Decoration, on_delete=models.SET_NULL, null=True, blank=True)
+    flooring = models.ForeignKey(Flooring, on_delete=models.SET_NULL, null=True, blank=True)
+    wall_type = models.ForeignKey(Wall, on_delete=models.SET_NULL, null=True, blank=True)
+    ceiling_type = models.ForeignKey(Ceiling, on_delete=models.SET_NULL, null=True, blank=True)
+
+    # Features & Amenities (Many-to-Many)
+    home_features = models.ManyToManyField(HomeFeature, blank=True)
+    project_amenities = models.ManyToManyField(ProjectAmenity, blank=True)
+
+    # Additional Property Conditions
+    conditions = models.ManyToManyField(PropertyCondition, blank=True)
+    views = models.ManyToManyField(View, blank=True)
+    warranties = models.ManyToManyField(Warranty, blank=True)
+
+    # Home Systems (Boolean Fields)
+    electrical_system = models.BooleanField(default=True)
+    air_conditioning = models.BooleanField(default=False)
+    hot_cold_water = models.BooleanField(default=False)
+    safety_circuit_breaker = models.BooleanField(default=True)
+    backup_power = models.BooleanField(default=False)
+    drainage_system = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
