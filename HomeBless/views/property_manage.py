ab1d31608ab401858917e706cdae814a61c559c4
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2025-04-03 03:26:39 +0700

    dfa

--- HomeBless/views/property_manage.py
+++ HomeBless/views/property_manage.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ..models import Property
 
 
-class PropertyManage(TemplateView):
+
+class PropertyManage(LoginRequiredMixin, TemplateView):
     template_name = 'property-manage.html'
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['properties'] = Property.objects.filter(user=self.request.user)
+        return context
 
     def post(self, request, *args, **kwargs):
+        property_id = request.POST.get('delete_property_id')
+        if property_id:
+            property_instance = get_object_or_404(Property, id=property_id, user=request.user)
+            property_instance.delete()
         return redirect('HomeBless:manage')
