c8dde8a3235d395cb15e84b7814deee8049f3ca6
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2025-04-03 21:30:38 +0700

    bookmark

--- HomeBless/views/property_listing.py
+++ HomeBless/views/property_listing.py
@@ -1,6 +1,8 @@
+from django.shortcuts import redirect
 from django.views.generic import ListView
 
-from ..models import Property
+from ..models import Property, Wishlist
+
 
 class PropertyListing(ListView):
     model = Property
@@ -9,7 +11,7 @@ class PropertyListing(ListView):
 
     def get_queryset(self):
         """Return only available properties sorted by newest"""
-        properties = Property.objects.filter(is_available=True) #.order_by('-created_at')
+        properties = Property.objects.filter(is_available=True)  # .order_by('-created_at')
 
         for property in properties:
             main_image = property.images.filter(is_main=True).first()
@@ -18,3 +20,29 @@ class PropertyListing(ListView):
             property.main_image = main_image.image.url if main_image else None
 
         return properties
+
+    def post(self, request, *args, **kwargs):
+        """Toggle property in wishlist."""
+        print("All POST data:", request.POST)
+
+        property_id = request.POST.get('property_id')
+
+        # Debug the received property_id
+        print('Received property_id:', property_id)
+
+        property = Property.objects.get(pk=property_id)
+
+        print('property:', property)
+
+        user = request.user
+
+        print('property:', property)
+        print('user:', user)
+
+        wishlist_item = Wishlist.objects.filter(user=user, property=property).first()
+        if wishlist_item:
+            wishlist_item.delete()
+        else:
+            Wishlist.objects.create(user=user, property=property)
+
+        return redirect('HomeBless:property-listing')
