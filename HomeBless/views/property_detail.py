1d36ddc6ea77d4fa3077e8f8a35a70f65a68cada
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2025-04-03 17:17:16 +0700

    nearby place featuer

--- HomeBless/views/property_detail.py
+++ HomeBless/views/property_detail.py
@@ -1,6 +1,8 @@
+import requests
 from django.shortcuts import redirect
 from django.views.generic import DetailView
 from ..models import Property
+from django.conf import settings
 
 
 class PropertyDetail(DetailView):
@@ -35,6 +37,25 @@ class PropertyDetail(DetailView):
         context['seller_line_id'] = self.object.line_id
         context['seller_email'] = self.object.contact_email
         context['selling_type'] = self.object.get_selling_type_display()
+
+        # Map API
+        context['google_maps_api_key'] = settings.GOOGLE_MAPS_API_KEY
+
+        # Nearby Places (Multiple Types)
+        universities = self.get_nearby_places("มหาวิทยาลัย")
+        filtered_universities = [uni for uni in universities if uni["name"].startswith("มหาวิทยาลัย")][:2]
+        context["nearby_university"] = filtered_universities
+        schools = self.get_nearby_places("secondary_school")
+        filtered_schools = [school for school in schools if
+                                 school["name"].startswith("โรงเรียน")][:2]
+        context['nearby_school'] = filtered_schools
+        airports = self.get_nearby_places("สนามบิน")
+        filtered_airports = [airport for airport in airports if
+                                 airport["name"].startswith("ท่าอากาศยาน")][:2]
+        context['nearby_airport'] = filtered_airports
+        context['nearby_mall'] = self.get_nearby_places("ศูนย์การค้า")[:3]
+        context['nearby_hospital'] = self.get_nearby_places("โรงพยาบาล")[:2]
+        context['nearby_transport'] = self.get_nearby_places("สถานีรถไฟฟ้า")[:2]
 
         # Decoration and Home Features
         context.update(self.get_property_tags())
@@ -77,6 +98,42 @@ class PropertyDetail(DetailView):
             'view_tags': self.object.views.all(),
             'warranty_tags': self.object.warranties.all(),
         }
+
+    def get_nearby_places(self, keyword):
+        """Fetch nearby places using Google Places API."""
+        lat = 13.84814
+        lng = 100.57223
+        google_api_key = settings.GOOGLE_MAPS_API_KEY
+
+        url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&rankby=distance&keyword={keyword}&language=th&key={google_api_key}"
+        response = requests.get(url)
+        data = response.json()
+
+        places = [
+            {
+                "name": place.get("name"),
+                "place_id": place.get("place_id"),
+                "location": f"{place['geometry']['location']['lat']},{place['geometry']['location']['lng']}",
+                "distance": '',
+            }
+            for place in data.get("results", [])
+        ]
+
+        # Prepare the destinations for the Distance Matrix API
+        destinations = "|".join([place["location"] for place in places])
+        distance_url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={lat},{lng}&destinations={destinations}&key={google_api_key}&language=th"
+
+        distance_response = requests.get(distance_url)
+        distance_data = distance_response.json()
+
+        # Add the distance data to the places list
+        for index, element in enumerate(
+            distance_data.get("rows", [])[0].get("elements", [])):
+            if "distance" in element:
+                places[index]["distance"] = element["distance"].get("text",
+                                                                    "N/A")
+
+        return places
 
     def post(self, request, *args, **kwargs):
         return redirect('HomeBless:property-detail', pk=self.get_object().pk)
