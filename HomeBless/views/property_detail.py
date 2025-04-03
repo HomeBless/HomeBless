58c33eeaba58fc74a87c0389ddfbafc123c51972
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2025-04-03 23:47:02 +0700

    make nearby place more accurate

--- HomeBless/views/property_detail.py
+++ HomeBless/views/property_detail.py
@@ -42,20 +42,52 @@ class PropertyDetail(DetailView):
         context['google_maps_api_key'] = settings.GOOGLE_MAPS_API_KEY
 
         # Nearby Places (Multiple Types)
-        universities = self.get_nearby_places("มหาวิทยาลัย")
+        universities = self.get_nearby_places("","มหาวิทยาลัย")
         filtered_universities = [uni for uni in universities if uni["name"].startswith("มหาวิทยาลัย")][:2]
         context["nearby_university"] = filtered_universities
-        schools = self.get_nearby_places("secondary_school")
+        schools = self.get_nearby_places("secondary_school", "")
         filtered_schools = [school for school in schools if
                                  school["name"].startswith("โรงเรียน")][:2]
         context['nearby_school'] = filtered_schools
-        airports = self.get_nearby_places("สนามบิน")
+        airports = self.get_nearby_places("สนามบิน", "สนามบิน")
         filtered_airports = [airport for airport in airports if
                                  airport["name"].startswith("ท่าอากาศยาน")][:2]
         context['nearby_airport'] = filtered_airports
-        context['nearby_mall'] = self.get_nearby_places("ศูนย์การค้า")[:3]
-        context['nearby_hospital'] = self.get_nearby_places("โรงพยาบาล")[:2]
-        context['nearby_transport'] = self.get_nearby_places("สถานีรถไฟฟ้า")[:2]
+        context['nearby_mall'] = self.get_nearby_places("shopping_mall" ,"ศูนย์การค้า")[:3]
+        hospitals = self.get_nearby_places("hospital", "โรงพยาบาล")
+        filtered_hospitals = [hospital for hospital in hospitals if hospital["name"].startswith("โรงพยาบาล")][:2]
+        context['nearby_hospital'] = filtered_hospitals
+        transports = self.get_nearby_places("สถานีรถไฟฟ้า", "สถานีรถไฟฟ้า")
+        filtered_transports = [transport for transport in transports
+                               if transport["name"] in
+                               ["คูคต", "แยก คปอ.", "พิพิธภัณฑ์กองทัพอากาศ",
+                                "โรงพยาบาลภูมิพลอดุลยเดช", "สะพานใหม่", "สายหยุด",
+                                "พหลโยธิน 59", "วัดพระศรีมหาธาตุ", "กรมทหารราบที่ 11",
+                                "บางบัว", "กรมป่าไม้", "มหาวิทยาลัยเกษตรศาสตร์",
+                                "เสนานิคม", "รัชโยธิน", "พหลโยธิน 24", "ห้าแยกลาดพร้าว",
+                                "หมอชิต", "สะพานควาย", "อารีย์", "สนามเป้า", "อนุสาวรีย์ชัยสมรภูมิ",
+                                "พญาไท", "ราชเทวี", "สยาม", "ชิดลม", "เพลินจิต", "นานา", "อโศก",
+                                "พร้อมพงษ์", "ทองหล่อ", "เอกมัย", "พระโขนง", "อ่อนนุช",
+                                "บางจาก", "ปุณณวิถี", "อุดมสุข", "บางนา", "แบริ่ง", "สำโรง", "ปู่เจ้า",
+                                "ช้างเอราวัณ", "โรงเรียนนายเรือ", "ปากน้ำ", "ศรีนครินทร์",
+                                "แพรกษา", "สายลวด", "เคหะฯ", "สนามกีฬาแห่งชาติ",
+                                "ราชดำริ", "ศาลาแดง", "ช่องนนทรี", "เซนต์หลุยส์", "สุรศักดิ์",
+                                "สะพานตากสิน", "กรุงธนบุรี", "วงเวียนใหญ่", "โพธิ์นิมิตร",
+                                "ตลาดพลู", "วุฒากาศ", "บางหว้า", "เจริญนคร (ไอคอนสยาม)",
+                                "คลองสาน", "ท่าพระ", "จรัญฯ 13", "ไฟฉาย", "บางขุนนนท์",
+                                "บางยี่ขัน", "สิรินธร", "บางพลัด", "บางอ้อ", "บางโพ",
+                                "เตาปูน", "บางซื่อ", "กำแพงเพชร", "สวนจตุจักร", "พหลโยธิน",
+                                "ลาดพร้าว", "รัชดาภิเษก", "สุทธิสาร", "ห้วยขวาง", "ศูนย์วัฒนธรรมแห่งประเทศไทย",
+                                "พระราม 9", "เพชรบุรี", "สุขุมวิท", "ศูนย์การประชุมแห่งชาติสิริกิติ์",
+                                "คลองเตย", "ลุมพินี", "สามย่าน", "หัวลำโพง", "วัดมังกร",
+                                "สามยอด", "สนามไชย", "อิสรภาพ", "บางไผ่", "เพชรเกษม 48",
+                                "ภาษีเจริญ", "บางแค", "หลักสอง", "รังสิต", "หลักหก (มหาวิทยาลัยรังสิต)",
+                                "ดอนเมือง", "การเคหะ", "หลักสี่", "ทุ่งสองห้อง", "บางเขน",
+                                "วัดเสมียนนารี", "กรุงเทพอภิวัฒน์", "คลองบางไผ่", "ตลาดบางใหญ่",
+                                "สามแยกบางใหญ่", "บางพลู", "บางรักใหญ่", "บางรักน้อยท่าอิฐ",
+                                "ไทรม้า", "สะพานพระนั่งเกล้า", "แยกนนทบุรี 1", "บางกระสอ",
+                                "กระทรวงสาธารณสุข", "แยกติวานนท์", "วงศ์สว่าง", "บางซ่อน"]]
+        context['nearby_transport'] = self.get_nearby_places("สถานีรถไฟฟ้า", "สถานีรถไฟฟ้า")[:2]
 
         # Decoration and Home Features
         context.update(self.get_property_tags())
@@ -99,13 +131,13 @@ class PropertyDetail(DetailView):
             'warranty_tags': self.object.warranties.all(),
         }
 
-    def get_nearby_places(self, keyword):
+    def get_nearby_places(self, type, keyword):
         """Fetch nearby places using Google Places API."""
         lat = self.object.latitude
         lng = self.object.longitude
         google_api_key = settings.GOOGLE_MAPS_API_KEY
 
-        url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&rankby=distance&keyword={keyword}&language=th&key={google_api_key}"
+        url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&rankby=distance&type={type}&keyword={keyword}&language=th&key={google_api_key}"
         response = requests.get(url)
         data = response.json()
 
