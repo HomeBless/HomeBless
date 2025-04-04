380c2aaf284e1e8d88d6a65a21dcfedd0613d955
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2025-04-04 13:04:07 +0700

    implement compare

--- HomeBless/views/compare.py
+++ HomeBless/views/compare.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from ..models.wishlist import Wishlist
 
 
 class Compare(TemplateView):
     template_name = 'compare.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        wishlist_items = Wishlist.objects.filter
+
+        context = {
+            'wishlist': wishlist_items,
+        }
+        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         return redirect('HomeBless:compare')
